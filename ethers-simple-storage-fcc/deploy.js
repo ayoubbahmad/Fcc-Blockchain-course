9d8a01d657881d9587b0d590325873f5bc9ad155
ayoubbahmad <ayoubbahmad@gmail.com>
2022-08-24 18:57:18 +0100

    add prettier configs

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -1,33 +1,33 @@
-const ethers = require('ethers');
-const fs = require('fs-extra');
+const ethers = require("ethers")
+const fs = require("fs-extra")
 
-require('dotenv').config();
+require("dotenv").config()
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
   const abi = fs.readFileSync(
-    './build/Simplestorage_sol_SimpleStorage.abi',
-    'utf-8'
-  );
+    "./build/Simplestorage_sol_SimpleStorage.abi",
+    "utf-8"
+  )
   const binary = fs.readFileSync(
-    './build/Simplestorage_sol_SimpleStorage.bin',
-    'utf-8'
-  );
+    "./build/Simplestorage_sol_SimpleStorage.bin",
+    "utf-8"
+  )
 
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log('Deploying, please wait...');
-  const contract = await contractFactory.deploy();
-  await contract.deployTransaction.wait(1);
-  console.log('Contract well deployed!');
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Deploying, please wait...")
+  const contract = await contractFactory.deploy()
+  await contract.deployTransaction.wait(1)
+  console.log("Contract well deployed!")
 
   //   Interact with abi
-  let currentFavNumber = await contract.retrieve();
-  console.log('Before: ', currentFavNumber.toString());
-  await contract.store(21);
-  currentFavNumber = await contract.retrieve();
-  console.log('After: ', currentFavNumber.toString());
+  let currentFavNumber = await contract.retrieve()
+  console.log("Before: ", currentFavNumber.toString())
+  await contract.store(21)
+  currentFavNumber = await contract.retrieve()
+  console.log("After: ", currentFavNumber.toString())
   // Define gas limite
   //   const contract = await contractFactory.deploy({ gasLimit: 10000000 });
 
@@ -57,6 +57,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
